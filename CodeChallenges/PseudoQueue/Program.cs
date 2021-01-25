6f33773768c24443fe1e0cb1c65f80d9f2082bdb
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-01-25 13:43:34 -0800

    fixed more file structure problems

---
+++ CodeChallenges/PseudoQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PseudoQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
