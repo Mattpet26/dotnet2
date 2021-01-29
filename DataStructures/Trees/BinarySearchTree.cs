3871b4bf35a604d8c52142b0b571d6a4572eed09
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-01-29 13:46:22 -0800

    Wrote binary tree, tree, BST. Implemented + Tested all functions. Passing all tests.

---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        /// <summary>
+        /// Traverse tree recursively, to find where node belongs.
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Node AddRecursively(Node current, int value)
+        {
+            if (current == null)
+            {
+                return new Node(value);
+            }
+            if (value < current.Value)
+            {
+                current.Left = AddRecursively(current.Left, value);
+            }
+            else if (value > current.Value)
+            {
+                current.Right = AddRecursively(current.Right, value);
+            }
+            else
+            {
+                return current;
+            }
+            return current;
+        }
+        public void add(int value)
+        {
+            Root = AddRecursively(Root, value);
+        }
+
+
+        public bool Find(int id)
+        {
+            Node current = Root;
+            while (current != null)
+            {
+                if (current.Value == id)
+                {
+                    return true;
+                }
+                else if (current.Value > id)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+            return false;
+        }
+    }
+}
