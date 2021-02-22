25426ad57d4bb6bdb8469c401c8fda632b569c5c
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-02-22 11:22:30 -0800

    Wrote RepeatedWord function with passing tests

---
+++ CodeChallenges/RepeatedWord/RepeatedWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepeatedWord
+{
+    public class RepeatedWord
+    {
+        public static string RepeatableWord(string words)
+        {
+            if (words == null) return null;
+            string[] wordArr = words.ToLower().Replace(",", "").Split(" ");
+
+            HashSet<string> set = new HashSet<string>();
+            foreach(string word in wordArr)
+            {
+                if (set.Contains(word))
+                {
+                    return word;
+                }
+                else
+                {
+                    set.Add(word);
+                }
+            }
+            return null;
+        }
+    }
+}
