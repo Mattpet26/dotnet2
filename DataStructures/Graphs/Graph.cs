fd0ae3006496ce3a34af6066ece9c5bcd48d2363
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-02-26 11:31:02 -0800

    wrote graph function with passing tests!

---
+++ DataStructures/Graphs/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs
+{
+    public class Graph
+    {
+        public List<GraphNode> nodes;
+
+    public Graph(){
+        nodes = new List<GraphNode>();
+    }
+
+    public GraphNode addNode(int value){
+        GraphNode node = new GraphNode(value);
+        nodes.Add(node);
+        return node;
+    }
+    public void addEdge(GraphNode source, GraphNode destination, int weight)
+        {
+        if(!nodes.Contains(source) || !nodes.Contains(destination)){
+            throw new Exception("Neither node was found in the graph, sorry");
+        }
+        GraphEdge edge = new GraphEdge(weight, destination);
+
+        if(source.edgeList == null){
+            source.edgeList = new List<GraphEdge>();
+        }
+        source.edgeList.Add(edge);
+    }
+    public List<GraphNode> getNodes(){
+        return nodes;
+    }
+    public List<GraphEdge> getNeighbors(GraphNode node){
+        return node.edgeList;
+    }
+    public int Size(){
+        return nodes.Count;
+    }
+
+
+
+    public class GraphNode{
+        public int value;
+        public List<GraphEdge> edgeList = new List<GraphEdge>();
+
+        public GraphNode(int value){
+            this.value = value;
+        }
+    }
+
+
+
+    public class GraphEdge {
+        public int weight;
+        public GraphNode node;
+
+        public GraphEdge(int weight, GraphNode node) {
+            this.weight = weight;
+            this.node = node;
+        }
+    }
+    }
+}
