37e68911b64a1f70dfafe99f06b3f1f994483876
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-03-03 13:49:24 -0800

    depth first, test broken. Function broken.

--- CodeChallenges/CodeChallengeTests/GraphSearchTests/UnitTest1.cs
+++ CodeChallenges/CodeChallengeTests/GraphSearchTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace GraphSearchTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestBreadthFirst()
         {
             Graph graph = new Graph();
             Graph.GraphNode one = graph.addNode(1);
@@ -26,6 +26,35 @@ namespace GraphSearchTests
             expectedValue.Add(three);
 
             Assert.Equal(expectedValue, GraphSearch.BreadthFirst.breadthFirst(one));
+        }
+        [Fact]
+        public void TestDepthFirst()
+        {
+            Graph graph = new Graph();
+            Graph.GraphNode one = graph.addNode(1);
+            Graph.GraphNode two = graph.addNode(2);
+            Graph.GraphNode three = graph.addNode(3);
+            Graph.GraphNode four = graph.addNode(4);
+            Graph.GraphNode five = graph.addNode(5);
+            Graph.GraphNode six = graph.addNode(6);
+
+            graph.addEdge(one, two, 9);
+            graph.addEdge(one, three, 5);
+            graph.addEdge(three, four, 1);
+            graph.addEdge(three, five, 1);
+            graph.addEdge(five, six, 1);
+            List<Graph.GraphEdge> oneEdges = graph.getNeighbors(one);
+            List<Graph.GraphEdge> twoEdges = graph.getNeighbors(two);
+
+            Stack<Graph.GraphNode> expected = new Stack<Graph.GraphNode>();
+            expected.Push(one);
+            expected.Push(two);
+            expected.Push(three);
+            expected.Push(five);
+            expected.Push(six);
+            expected.Push(four);
+
+            Assert.Equal(expected, GraphSearch.DepthFirst.DFS(one));
         }
     }
 }
---
+++ CodeChallenges/GraphSearch/DepthFirst.cs
@@ -0,0 +1,37 @@
+﻿using Graphs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphSearch
+{
+    public class DepthFirst
+    {
+        public static List<Graph.GraphNode> DFS(Graph.GraphNode start)
+        {
+            Stack<Graph.GraphNode> Stack = new Stack<Graph.GraphNode>();
+            HashSet<Graph.GraphNode> Seen = new HashSet<Graph.GraphNode>();
+            List<Graph.GraphNode> result = new List<Graph.GraphNode>();
+
+            Stack.Push(start);
+
+            while (Stack.Count != 0)
+            {
+                Graph.GraphNode current = Stack.Pop();
+                if (!Seen.Contains(current))
+                {
+                    Seen.Add(current);
+                }
+                foreach (Graph.GraphNode adjacent in Stack)
+                {
+                    if (!Seen.Contains(adjacent))
+                    {
+                        Stack.Push(adjacent);
+                    }
+                }
+                result.Add(current);
+            }
+            return result;
+        }
+    }
+}
